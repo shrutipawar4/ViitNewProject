fc75c87d7011f19c89be5f227354c1bfa46279d2
shrutipawar4 <shrutipawar509@gmail.com>
2025-07-30 16:04:00 +0530

    Fix casing or code in NewsItems.jsx

--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NewsItems = ({ title, description, src, url }) => {
+const newsItems = ({ title, description, src, url }) => {
   return (
     <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 py-3 px-3" style={{width: "300px" }}>
       <img src={src || "https://via.placeholder.com/300x200"} className="card-img-top" alt="news" style={{ height: "200px", width: "100%" }} />
@@ -13,4 +13,4 @@ const NewsItems = ({ title, description, src, url }) => {
   );
 };
 
-export default NewsItems;
+export default newsItems;
